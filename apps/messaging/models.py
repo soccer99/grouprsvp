16464706f954e1e5c2da18877414fb05a4232bfb
Brady Endres <bfendres@gmail.com>
2019-05-29 10:03:16 -0700

    migrations

--- apps/messaging/models.py
+++ apps/messaging/models.py
@@ -36,6 +36,7 @@ class SMSDR(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
+
 class EmailMessage(models.Model):
     email = models.CharField(max_length=120)
     content = models.TextField(blank=True)
